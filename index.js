cf7f07ccdb4d887b273f66effe73744dafeb7ea1
Carlos Henrique <rapaiva21@gmail.com>
2021-12-06 10:37:28 -0300

    Método transferir está funcionando normalmente.

--- index.js
+++ index.js
@@ -8,16 +8,24 @@ class ContaCorrente {
     cliente;
     _saldo = 0;
 
-    // SEM VER -- CRIAÇÃO DOS MÉTODOS sacar() depositar() e transferir()
+    // SEM VER -- CRIAÇÃO DOS MÉTODOS sacar() depositar()
+    // SEM VER -- CRIAÇÃO DE UM NOVO CLIENTE E UMA NOVA CONTA E MÉTODO transferir()
 
     sacar(valor){
-        if(this._saldo < valor) return;
+        if(this._saldo >= valor){
         this._saldo -= valor;
+        return valor;
+        }
     }
     depositar(valor){
-        if(valor > 0){
-            return this._saldo += valor; 
+        if(valor <= 0){ 
+            return;
         }
+            this._saldo += valor;
+    }
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
 
@@ -29,8 +37,16 @@ const contaCorrenteRicardo = new ContaCorrente()
 contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo._saldo = 100;
-contaCorrenteRicardo.sacar(51);
-contaCorrenteRicardo.depositar(151);
 
+const cliente2 = new Cliente()
+cliente2.nome = "Alice";
+cliente2.cpf = 0001112224;
 
+const conta2 = new ContaCorrente()
+conta2.agencia = 1002;
+conta2.cliente = cliente2;
+conta2._saldo = 500;
+
+conta2.transferir(150, contaCorrenteRicardo)
+console.log(conta2._saldo)
 console.log(contaCorrenteRicardo)

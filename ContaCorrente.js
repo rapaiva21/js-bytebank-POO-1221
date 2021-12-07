ee519bf74cd327f49725f5905dac4928d9e09f3a
Carlos Henrique <rapaiva21@gmail.com>
2021-12-06 22:38:30 -0300

    Foi adicionado os métodos da classe ContaCorrente para manipualção de valores. sacar(). depositar() e transferir()

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -3,5 +3,24 @@ export class ContaCorrente {
     cliente;
     _saldo = 0;
 
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+        this._saldo += valor;
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+
 
 }

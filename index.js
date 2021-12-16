bacbe59dcc1b873c31cabd5cf4df12850495dbf4
Carlos Henrique <rapaiva21@gmail.com>
2021-12-15 23:24:50 -0300

    Primeiramente, está sendo impresso o numero de contas por meio de um atributo estático. No arquivo, também está tudo referenciado ao arquivo e classe Conta.js. Que será o arquivo base para os tipos de conta na plataforma.

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import { Cliente } from "./Cliente.js"
 import { ContaCorrente } from "./ContaCorrente.js"
+import { ContaPoupanca } from "./ContaPoupanca.js"
+import { Conta } from "./Conta.js"
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const contaCorrenteRicardo = new Conta(1001, cliente1);
 
 const cliente2 = new Cliente("Alice", 88822233309);
-const conta2 = new ContaCorrente(1002, cliente2);
+const conta2 = new Conta(1002, cliente2);
 
-console.log(contaCorrenteRicardo);
-console.log(conta2);
+const cliente3 = new Cliente("José", 44455566621);
+const conta3 = new Conta(1003, cliente3);
+
+const contaPoupanca = new Conta(50, cliente1, 1001);
+
+contaCorrenteRicardo.depositar(500);
+conta2.depositar(500);
+
+console.log(Conta.numeroDeContas);
+console.log(contaPoupanca);

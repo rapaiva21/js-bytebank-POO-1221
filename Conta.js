538ba9fc047e0e09683e61bfba4927d4f873d6dd
Carlos Henrique <rapaiva21@gmail.com>
2022-01-07 22:18:14 -0300

    A classe mãe Conta conta com as informações no qual os tipos de conta utilizarão para herdar.

--- Conta.js
+++ Conta.js
@@ -5,8 +5,22 @@ export class Conta{
         this._agencia = agencia;
     }
 
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
     sacar(valor) {
-        taxa = 1.1 * valor;
+        let taxa = 1.1 * valor;
         if(this._saldo >= valor) {
             this._saldo -= valor;
             return valor;

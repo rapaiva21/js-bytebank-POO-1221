7067b617caf31683ba5ed3686ce0c42dcd6f398e
Carlos Henrique <rapaiva21@gmail.com>
2022-01-08 18:57:47 -0300

    Foi adicionada uma linha para testar a o uso da classe Conta, que agora é uma classe astrata: Não pode ser instanciada, somente herdada. Ela tem o comportamento padrão para cada tipo de conta.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import {ContaPoupanca} from "./ContaPoupanca.js";
+import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
@@ -9,6 +10,7 @@ contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+// const conta = new Conta(0, cliente1, 1001);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);

739ea1a8fba2dfd7b7aa7d6282a88ebf85a4f280
Carlos Henrique <rapaiva21@gmail.com>
2022-01-07 21:21:52 -0300

    arquivo index mais simplificado para testes com Conta.js

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-import { Cliente } from "./Cliente.js"
-import { ContaCorrente } from "./ContaCorrente.js"
-import { ContaPoupanca } from "./ContaPoupanca.js"
-import { Conta } from "./Conta.js"
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
+import {ContaPoupanca} from "./ContaPoupanca.js";
+import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
-const contaCorrenteRicardo = new Conta(1001, cliente1);
 
-const cliente2 = new Cliente("Alice", 88822233309);
-const conta2 = new Conta(1002, cliente2);
-
-const cliente3 = new Cliente("José", 44455566621);
-const conta3 = new Conta(1003, cliente3);
+const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
 const contaPoupanca = new Conta(50, cliente1, 1001);
 
-contaCorrenteRicardo.depositar(500);
-conta2.depositar(500);
-
-console.log(Conta.numeroDeContas);
 console.log(contaPoupanca);
+console.log(contaCorrenteRicardo);

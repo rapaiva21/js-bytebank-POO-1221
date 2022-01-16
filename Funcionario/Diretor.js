3f9c582c48c61f07f3e09af0c10ec52ff2996b25
Carlos Henrique <rapaiva21@gmail.com>
2022-01-16 07:40:38 -0300

    Reparando um erro ao criar a classe Diretor. Tinha nomerado erroneamente de Gerente.

--- Funcionario/Diretor.js
+++ Funcionario/Diretor.js
@@ -1,6 +1,6 @@
 import { Funcionario } from "./Funcionario.js"
 
-export class Gerente extends Funcionario{
+export class Diretor extends Funcionario{
     constructor(nome, salario, cpf){
         super(nome, salario, cpf)  
         this._bonificacao = 2;      

500ccc64b7964980ec4be90eb1cb2d913f7e769d
Carlos Henrique <rapaiva21@gmail.com>
2022-01-19 16:40:41 -0300

    teste na autenticação do gerente, do diretor e do cliente.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {Cliente} from "./Cliente.js";
-import {Gerente} from "./Funcionario/Gerente.js"
-import {Diretor} from "./Funcionario/Diretor.js"
+import {Gerente} from "./Funcionario/Gerente.js";
+import {Diretor} from "./Funcionario/Diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
@@ -8,8 +8,11 @@ diretor.cadastrarSenha("123456");
 const gerente = new Gerente("Ricardo", 5000, 12378945601);
 gerente.cadastrarSenha("123");
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456");
-const estaLogado2 = SistemaAutenticacao.login(gerente, "123");
-console.log(estaLogado);
-console.log(estaLogado);
+const cliente = new Cliente("Lais", 78945612379, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+console.log(gerenteEstaLogado, diretorEstaLogado);
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+console.log(clienteEstaLogado);
 
